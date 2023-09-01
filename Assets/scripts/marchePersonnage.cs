474dd45a5c7b64e79e8d9a46c76f3fb43f795f25
unknown <belamard@gmail.com>
2023-09-01 03:32:15 +0200

    animation et gestion de la camera

--- Assets/scripts/marchePersonnage.cs
+++ Assets/scripts/marchePersonnage.cs
@@ -23,28 +23,28 @@ public class marchePersonnage : MonoBehaviour
         float moveHorizontal=Input.GetAxis("Horizontal");
         float moveVertical=Input.GetAxis("Vertical");
         Vector2 move= new Vector2(moveHorizontal, moveVertical);
-        rb.velocity= move*speed;
+        rb.velocity= move*speed*Time.fixedDeltaTime;
         makeAnimation(moveHorizontal, moveVertical);
         
     }
     void makeAnimation(float dirx, float diry){
-        if(dirx==0 && diry==0)
+        if(dirx==0 && diry==0)// idle
         {
             anim.SetInteger("dir", 0);
         }
-        else if(dirx>0)
+        else if(dirx>0)//right
         {
             anim.SetInteger("dir", 2);
         }
-        else if(dirx<0)
+        else if(dirx<0)//left
         {
             anim.SetInteger("dir", 1);
         }
-        else if(diry>0)
+        else if(diry>0)//up
         {
             anim.SetInteger("dir", 3);
         }
-        else if(diry<0)
+        else if(diry<0)//down
         {
             anim.SetInteger("dir", 4);
         }
