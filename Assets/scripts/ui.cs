2dcb291e16a8da92a794e1fbe7fcc7335e858d49
unknown <belamard@gmail.com>
2023-09-04 16:05:52 +0200

    feat(fr):formation de jeu menu demarrer

---
+++ Assets/scripts/ui.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ui : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] 
+    int time;
+    [SerializeField]
+    Text MyTime;
+    void Start()
+    {
+        
+       StartCoroutine(Minuterie());
+    }
+    IEnumerator Minuterie(){
+       
+        while(time>0){
+            yield return new WaitForSeconds(1f);
+            time--;
+            Debug.Log("Minuterie du temps ");
+            MyTime.text="ngonga:"+time;
+        }
+        
+        
+    }
+
+    // Update is called once per frame
+
+}
